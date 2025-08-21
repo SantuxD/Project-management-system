615f6309f81b2eb66c5416d9fc80483395108416
Santu Mondal <mondalsantu104@gmail.com>
2025-08-21 13:17:45 +0530

    MongoDB atlas connection created

--- src/index.js
+++ src/index.js
@@ -2,26 +2,35 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.port || 3000;
-
+const connectDB = require("./db/index.js");
+const port = process.env.PORT || 3000;
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:8000",
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-
-}));
-
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the Project Management System API");
+  res.send("Welcome to the Project Management System API");
+});
 
-})
+connectDB()
+  .then(() => {
+    console.log("Database connection established successfully");
+  })
+
+  .catch((err) => {
+    console.error("Failed to connect to the database:", err.message);
+    process.exit(1); // Exit the process with failure
+  });
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
